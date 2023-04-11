1423644b49367754570ac66425c2e74cc720932d
cvache <vachecarson@seattleu.edu>
2023-04-10 22:56:11 -0400

    update gitignore

--- src/main.py
+++ src/main.py
@@ -7,13 +7,61 @@ class TrackerItem(BaseModel):
     level: str
     timestamp: str | None = None
 
+eventTracker = {}
+inProgress = []
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.put("/tracker")
+@app.post("/tracker")
 async def tracker(trackerData: TrackerItem):
-    return trackerData
+    id = trackerData.id
+    level = trackerData.level
+    timestamp = trackerData.timestamp
+    
+    #add start
+    if level == "START":
+        inProgress.append(id)
+        eventTracker[id] = []
+        eventTracker[id].push({
+        "level": level,
+        "timestamp": timestamp
+        })
+        return {
+            "statusCode": "200",
+            "message": "Operation started"
+        }
+
+    if id not in inProgress:
+        return {
+            "statusCode": "404",
+            "message": "Operation is not being tracked. Has it been started?"
+        }
+    
+    eventTracker[id].push({
+        "level": level,
+        "timestamp": timestamp
+    })
+
+    if level == "END":
+        payload = eventTracker[id].reverse()
+        inProgress.remove(id)
+        eventTracker.pop(id)
+
+        return {
+            "statusCode": "200",
+            "message": "Operation finished"
+        }
+
+
+        
+        
+    return {
+        "statusCode": "200",
+        "message": "Operation added"
+    }
+
 
